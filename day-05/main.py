223ce51d79ba9ae9640f898b7e0e350a8a12c8bc
anquetos <t.anquetil@proton.me>
2024-12-06 06:45:48 +0100

    Part two wip

--- day-05/main.py
+++ day-05/main.py
@@ -41,3 +41,34 @@ for page_update in page_updates:
 
 print("--- Part One ---")
 print(f'Number of "XMAS" : {middle_page_sum}.\n')
+
+# --- Part Two ---
+
+text= "75,97,47,61,53"
+test = [num for num in text.split(',')]
+
+rules_t = "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13"
+rules = [r.split("|") for r in rules_t.split()]
+
+print(rules)
+
+combi = [list(combination) for combination in combinations(test, 2)]
+print(combi)
+
+
+for pair in combi:
+    if pair in rules:
+        print("ok")
+    elif pair[::-1] in rules:
+        print("reverse_ok")
+        x = pair[0]
+        y = pair[1]
+        print("number to switch", pair[0], pair[1])
+        
+
+        x_idx = test.index(x)
+        y_idx = test.index(y)
+
+        test[x_idx] = y
+        test[y_idx] = x
+print(test)
