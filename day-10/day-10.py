1f2fd569295e3c288f9b4220981723258be1f418
anquetos <t.anquetil@proton.me>
2024-12-29 19:44:06 +0100

    Day 10 part on completed

--- day-10/day-10.py
+++ day-10/day-10.py
@@ -7,43 +7,80 @@ q = p / INPUT_FILENAME
 
 # Read input file in a list
 with open(q) as f:
-    input = f.readlines()
+    lines = f.readlines()
 
-topographic_map = [list(map(int, line.strip())) for line in input]
+topographic_map = [list(map(int, line.strip())) for line in lines]
 
 
-# Get trailheads (0) coordinates
-def get_trailheads_coordinates(
-    topographic_map: list[list[int]], trailhead_number: int = 0
-):
+def check_coordinates_validity(
+    matrix: list[list], coordinates: tuple[int, int]
+) -> bool:
+    max_x, max_y = len(matrix[0]) - 1, len(matrix) - 1
+    x, y = coordinates
+
+    if 0 <= x <= max_x and 0 <= y <= max_y:
+        return True
+
+    return False
+
+
+def get_position_height(matrix: list[list], coordinates: tuple[int, int]) -> int:
+    x, y = coordinates
+    if check_coordinates_validity(matrix, (x, y)):
+        return matrix[y][x]
+    return None
+
+
+def get_trailheads_coordinates(matrix: list[list[int]], height: int = 0):
     trailheads = []
-    for i, row in enumerate(topographic_map):
-        if trailhead_number in row:
-            trailheads.append((row.index(trailhead_number), i))
+    for y, row in enumerate(matrix):
+        for x, value in enumerate(row):
+            if value == height:
+                trailheads.append((x, y))
     return trailheads
 
 
+# Get trailheads
 trailheads = get_trailheads_coordinates(topographic_map)
 print(f"Trailheads coordinates : {trailheads}\n")
 
-# print([i for i in range(1, 10)])
 
-# Look for 1 in all directions [(1, 0), (0, 1), (-1, 0), (0, -1)]
-# If find 1, look for two in all directions
+def get_valid_step_positions(
+    matrix: list[list[int]],
+    current_positions: list[tuple[int, int]],
+    current_height: int,
+) -> list[tuple[int, int]]:
+    valid_step_positions = set()
 
-directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    for current_position in current_positions:
+        cx, cy = current_position
 
-x, y = trailheads[2]
-print("Test", x, y)
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        for dx, dy in directions:
+            nx, ny = cx + dx, cy + dy
+            next_height = get_position_height(matrix, (nx, ny))
 
-value = 0
-for dx, dy in directions:
-    nx, ny = (x + dx, y + dy)
-    if topographic_map[ny][nx] == value + 1:
-        pass
-        # save coord
+            if (
+                check_coordinates_validity(matrix, (nx, ny))
+                and next_height is not None
+                and (next_height - current_height == 1)
+            ):
+                valid_step_positions.add((nx, ny))
+
+    return list(valid_step_positions)
+
+
+scores_sum = 0
+for trailhead in trailheads:
+    positions = [trailhead]
+
+    for height in range(9):
+        valid_steps = get_valid_step_positions(topographic_map, positions, height)
+        positions = valid_steps
+
+    scores_sum += len(positions)
 
 # --- Part One ---
 
 print("--- Part One ---")
-print(f"Result : {None}\n")
+print(f"Sum of the scores of all trailheads : {scores_sum}\n")
